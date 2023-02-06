f957154088fd9d9b2b95170ef7d17000a3dc4bf0
GIGABAIT <59637348+GIGABAIT93@users.noreply.github.com>
2023-02-06 11:11:50 +0200

    Update panel-update.sh

--- Util/panel-update.sh
+++ Util/panel-update.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# bash < <(curl -s https://raw.githubusercontent.com/Mubeen142/billing-docs/main/Util/panel-update.sh)
 curl -L https://github.com/pterodactyl/panel/releases/latest/download/panel.tar.gz | tar -xzv
 chmod -R 755 storage/* bootstrap/cache
 echo "yes" | composer install --no-dev --optimize-autoloader
